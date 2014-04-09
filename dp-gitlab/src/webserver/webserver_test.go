a43090c8428aa431c58f2460176d74896d6368ac
noahshen <Noah.Shen87@gmail.com>
2014-04-09 23:06:06 +0800

    remove proxy

--- dp-gitlab/src/webserver/webserver_test.go
+++ dp-gitlab/src/webserver/webserver_test.go
@@ -6,8 +6,6 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	os.Setenv("HTTP_PROXY", "http://peiqi.shen:159357Dp87@remoteapp.dianping.com:808")
-	os.Setenv("HTTPS_PROXY", "http://peiqi.shen:159357Dp87@remoteapp.dianping.com:808")
 
 	StartServer()
 }
