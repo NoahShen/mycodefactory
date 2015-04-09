1c5c802dfa522d1abdd1f63c539b9ddd6fccbc6b
peiqi.shen <peiqi.shen@dianping.com>
2015-04-09 20:36:23 +0800

    rename

--- akkaTest2/src/main/groovy/org/github/noahshen/mycodefactory/akkatest/Main.groovy
+++
@@ -1,8 +0,0 @@
-package org.github.noahshen.mycodefactory.akkatest
-
-public class Main {
-
-  public static void main(String[] args) {
-    akka.Main.main([HelloWorld.class.name] as String[]);
-  }
-}
